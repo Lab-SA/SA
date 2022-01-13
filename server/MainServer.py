76d527def07ebf4c13e4949dfa01d37def71f734
0JIEUN0 <jieun4436@naver.com>
2022-01-13 22:53:42 +0900

    Server: add 'foreach' response and add support for Round1(:ShareKeys)

--- server/MainServer.py
+++ server/MainServer.py
@@ -34,9 +34,8 @@ class MainServer:
                     print('[{0}] Client request: {1}'.format(self.tag, request))
                     self.userNum = self.userNum + 1
 
-                    # request example: {"c_pk":"VALUE", "s_pk": "VALUE"}
-                    requestDic = json.loads(request)
-                    self.requests.append((clientSocket, requestDic))
+                    requestData = json.loads(request)
+                    self.requests.append((clientSocket, requestData))
                 except socket.timeout:
                     pass
                 
@@ -47,10 +46,23 @@ class MainServer:
             print('[{0}] Exception: insufficient {1} users with {2} threshold'.format(self.tag, self.userNum, self.t))
             raise Exception("Need at least {0} users, but only {1} user(s) responsed".format(self.t, self.userNum))
     
+    # broadcast to all client (same response)
     def broadcast(self, response): # send response (broadcast)
         response_json = json.dumps(response)
         for client in self.requests:
             clientSocket = client[0]
+            clientSocket.sendall(bytes(response_json, self.ENCODING))
+        
+        self.serverSocket.close()
+        print('[{0}] Server finished'.format(self.tag))
+    
+    # send response for each client (different response)
+    def foreach(self, response):
+        # 'response' must be: { index: [response], ... }
+        # 'requestData' must be: [ index, [...data] ... ]
+        for (clientSocket, requestData) in self.requests:
+            idx = requestData[0]
+            response_json = json.dumps(response[idx])
             clientSocket.sendall(bytes(response_json, self.ENCODING))
         
         self.serverSocket.close()

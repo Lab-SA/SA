be6358d3a79971a8a06a49b8c3264f6ece6ecb4c
sub2nnj5 <77657793+QqwweerrttyyU@users.noreply.github.com>
2022-01-14 19:31:26 +0900

    basicSA : add quantization

--- BasicSA.py
+++ BasicSA.py
@@ -82,5 +82,100 @@ def generatingOutput(yu_list, pu_list, puv_list):
     sum_xu = sum(yu_list) - sum(pu_list) + sum(puv_list)
     return sum_xu
 
+def stochasticQuantization(x, q, p):
+    # x = local model of user i
+    # q = quantization level
+    # p = modulo p
+
+    # var_x = quantized x
+    var_x = representNegativeInt(q * stochasticRounding(x, q), p)
+    return var_x
+
+def stochasticRounding(x, q):
+    # x = local model of user i
+    # q = quantization level
+
+    # ret_list = return value list after rounding x
+    # prob_list = probability list (weight for random.choices)
+    # ret_x = rounded x
+
+    ret_list = [math.floor(q * x) / q, (math.floor(q * x) + 1) / q]
+    prob_list = [1 - (q * x - math.floor(q * x)), q * x - math.floor(q * x)]
+    ret_x = random.choices(ret_list, prob_list)
+    return ret_x
+
+def mapping(x, p):
+    """mapping(x, p) is to represent a negative integer in the finite field
+    by using two’s complement representation"""
+    # % mod 연산으로 대체가 가능한 부분인가...?
+    if x < 0:
+        return p + x
+    else:
+        return x
+
+def SS_Matrix(G):
+    # G = the number of groups
+    B = [['*' for x in range(G)] for y in range(G)]
+    for g in range(G - 1):
+        for r in range(G - g - 1):
+            l = 2 * g + r
+            B[l % G][g] = g
+            B[l % G][g + r + 1] = g
+    for i in range(G):
+        for j in range(G):
+            if B[i][j] == '*':
+                B[i][j] = j
+    return B
+
+# generate G quantizers randomly
+def generateQuantizers_heteroQ(G):
+    Q = []
+    for i in range(G):
+        Q.append(random.randint(1, 100))
+    return Q
+
+# return quantization level q
+def returnQuantizer(mode, l, g, B, Q):
+    # mode = homo quantization(0) or hetero quantization(1)
+    # l = segment index
+    # g = group index
+    # B = SS_Matrix
+    # Q = a set of quantization level
+
+    # q = quantization level
+    if mode == 0:  # homo quantization
+        q = Q[0]
+        return q
+    elif mode == 1:  # hetero quantization
+        q = Q[B[l][g]]
+        return q
+    else:
+        print("wrong input")
+        return 0
+
+def Quantization(x, G, q, r1, r2):
+    # x = local model value of user i
+    # G = the number of groups
+    # q = quantization level
+    # [r1, r2] = quantization range
+    """ q = returnQuantizer(mode, l, g, B, Q)
+    => Quantization(x, G, q) can do both homo quantization and hetero quantization. """
+
+    # delK = quantization interval
+    # T = discrete value from quantization range
+    delK = (r2 - r1) / (q - 1)
+    T = []
+    for l in range(0, G):
+        T.append(r1 + l * delK)
+
+    for l in range(0, G - 1):
+        if T[l] <= x < T[l + 1]:
+            ret_list = [T[l], T[l+1]]
+            prob_list = [(x - T[l]) / (T[l + 1] - T[l]), 1-((x - T[l]) / (T[l + 1] - T[l]))]
+            ret_x = random.choices(ret_list, prob_list)
+            break
+    return ret_x
+
+
 # just for testing
 # if __name__ == "__main__":

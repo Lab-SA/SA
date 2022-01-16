a1c92940086240313698cbd0d15d95c1c012f883
eigenkyeong <76428047+eigenkyeong@users.noreply.github.com>
2022-01-16 18:28:04 +0900

    Server: Round2, Round4

--- server/BasicSAServer.py
+++ server/BasicSAServer.py
@@ -46,6 +46,69 @@ def shareKeys():
     
     server.foreach(response)
 
+    
+def MaskedInputCollection():
+    server = MainServer('MaskedInputCollection')
+    server.start()
+
+    # if u3 dropped
+    # requests example: { (0, y1), (1, y2), (3, y3), ... }
+    requests = server.requests
+
+    # response example: { 0, 1, 3, ... }
+    response = {}
+    for i, request in enumerate(requests):
+        requestData = request[1]  # (socket, data)
+        response[i] = requestData[0]
+        # server.yu = server.yu + requestData[1]
+
+    server.broadcast(response)
+
+def Unmasking():
+    server = MainServer('Unmasking')
+    server.start()
+
+    # if u2, u3 dropped
+    # requests example: { [{0: s02_sk, 1: s03_sk, ...}, {1: b01, 4: b04, ...}], ... }
+    requests = server.requests
+
+    drop_usr = {}       # {0: {0: s02_sk, 1: s03_sk, ...}, 1: {0: s12_sk, 1: s13_sk, ...}, ... }
+    survive_usr = {}    # {0: {1: b01, 4: b04, ...}, 1: {0: b10, 4: b14, ...}, ... }
+    share_list = {}     # {0: [s02_sk, s12_sk, ... ], 1: [s03_sk, s13_sk, ... ], ... }
+    bu_list = {}        # {0: [b10, b40, ... ], 1: [b01, b41, ... ], 2: [], ... }
+    requestData = []
+
+    for request in requests:
+        requestData.append(requests[request])
+        for i, data in enumerate(requestData):
+            drop_usr[i] = data[0]
+            survive_usr[i] = data[1]
+
+    surv_num = len(drop_usr)
+    drop_num = server.userNum - len(drop_usr)
+    for n in range(drop_num):
+        share_list[n] = []
+    for n in range(server.userNum):
+        bu_list[n] = []
+    for n in range(drop_num):
+        for i in range(surv_num):
+            share_list[n].append(drop_usr[i][n])
+    for n in range(server.userNum):
+        for i in range(surv_num):
+            if n in survive_usr[i]:
+                bu_list[n].append(survive_usr[i][n])
+
+    # su_sk = sp.reconstruct(share_list)
+    # recompute p_vu
+
+    # bu = sp.reconstruct(bu_list)
+    # recompute p_u
+
+    # z = sum(yu) - sum(p_u) - sum(p_vu)
+    
+    
 if __name__ == "__main__":
     advertiseKeys() # Round 0
     shareKeys() # Round 1
+    MaskedInputCollection() # Round 2
+    Unmasking() # Round 4

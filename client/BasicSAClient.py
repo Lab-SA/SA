1d4e3e09db5350dc23554fcccc1bb2f0d1fc7325
sub2nnj5 <77657793+QqwweerrttyyU@users.noreply.github.com>
2022-01-17 14:01:48 +0900

    Client : change BasicSAClient
    class MainClient 에 정의했던 변수를 BasicSAClient로 옮겨왔습니다.
    각 함수마다 response를 return 하도록 추가했습니다.
    각 라운드마다 server response 수신받는 방식으로 변경하였습니다.

--- client/BasicSAClient.py
+++ client/BasicSAClient.py
@@ -1,46 +1,51 @@
 import json
 import socket
-import time
-from MainClient import MainClient
+
+
+HOST, PORT = 'localhost', 7
+SIZE = 1024
+ENCODING = 'ascii'
 
 def advertiseKeys(c_pk, s_pk):
     request = {"c_pk": c_pk, "s_pk": s_pk}  # temp
-    # response = {}
+    response = {}
 
-    client = MainClient('AdvertiseKeys')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((client.HOST, client.PORT))
+    s.connect(HOST, PORT)
     print("connected!")
 
     # send (c_pk, s_pk) to server in json format
-    s.sendall(bytes(json.dumps(request), client.ENCODING))
+    s.sendall(bytes(json.dumps(request), ENCODING))
     print(f"[Client] send request {request}")
 
+    # receive other users' public keys from server in json format
+    response = json.loads(s.recv(SIZE, ENCODING))
+    print(f"[Client] receive response {response}")
+
     s.close()
+
+    return response
 
 
 def shareKeys(euv):
     # euv(encrypted shares of bu and s_sk)
     request = euv  # temp. [] format
-    response = {}
+    response = []
 
-    client = MainClient('ShareKeys')
-    client.startTime = client.endTime = time.time()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((client.HOST, client.PORT))
+    s.connect(HOST, PORT)
     print("connected!")
 
-    # receive other users' public keys from server in json format
-    while (client.endTime - client.startTime) < client.interval:
-        try:
-            response = json.loads(s.recv(client.SIZE, client.ENCODING))
-            print(f"[Client] receive response {response}")
-        except socket.timeout:
-            pass
-        client.endTime = time.time()
-
-    # send euv(encrypted shares of bu and s_sk) to server
-    s.sendall(bytes(request, client.ENCODING))
+    # send euv(encrypted shares of bu and s_sk) to server in list format
+    s.sendall(bytes(request, ENCODING))
     print(f"[Client] send request {request}")
 
+    # receive euv_list from server in json format
+    # change euv_list format from json to list and store
+    response = json.loads(s.recv(SIZE, ENCODING))
+    print(f"[Client] receive response {response}")
+
     s.close()
+
+    return response
+

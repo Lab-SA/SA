7d33a8010d10b1d4d37a3c50cc69496b9a5148af
eigenkyeong <76428047+eigenkyeong@users.noreply.github.com>
2022-02-03 03:14:56 +0900

    Add parameter

--- vss.py
+++ vss.py
@@ -10,19 +10,20 @@ cld = [0, 2, 3]     # collude 사용자 인덱스 리스트
 list = [1, 2, 3]    # 0번 user인 경우
 
 # make N theta_i
-def make_theta():   # Server
+def make_theta(n, q):   # Server
     th = []
     for i in range(n):
         th.append(random.randint(1, q))
     return th
 
 # Secret polynomium coefficients r_ij
-def get_rij():
+def get_rij(n, q):
+    rij = []
     for j in range(n):
-        rij.append(random.randint(1,q))
+        rij.append(random.randint(1, q))
     return rij
 
-def f(x, w):
+def f(x, w, cld, rij, q):
     y = w
     for i in range(len(cld)):
         y = y + rij[cld[i]] * (x ** (i+1))
@@ -32,11 +33,11 @@ def f(x, w):
 
 # make shares
 def vss(list, w, x):
-    t = make_theta()
-    rij = get_rij()
+    t = make_theta(n, q)
+    rij = get_rij(n, q)
     s = []
     for j in list:
-        s.append(f(t[j], w))
+        s.append(f(t[j], w, cld, rij, q))
     return s
   
   

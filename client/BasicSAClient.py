2bceaa26da04bfb56ee4c4441c83ff8f44a1ccdd
Sub1nJ0 <77657793+Sub1nJ0@users.noreply.github.com>
2022-02-04 09:37:20 +0900

    Client : add Round4(Unmaskng),  etc..
    1. add round 4 && change response in round 1 for U2
    2. user index u
    3. add function parameter

--- client/BasicSAClient.py
+++ client/BasicSAClient.py
@@ -11,10 +11,10 @@ commonValues = {}  # {"n": n, "t": t, "g": g, "p": p, "R": R} from server in set
 u = 0  # u = user index
 my_keys = {}  # c_pk, c_sk, s_pk, s_sk of this client
 others_keys = {}  # other users' public key dic
-euv_list = [] # euv of this client
+euv_list = []  # euv of this client
 others_euv = {}
-bu = 0 # random element to be used as a seed for PRG
-others_yu = {}
+bu = 0  # random element to be used as a seed for PRG
+U3 = []  # survived users in round2(MaskedInputCollection)
 
 def setUp():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,10 +24,6 @@ def setUp():
     # {"n": n, "t": t, "g": g, "p": p, "R": R}
     commonValues = json.loads(s.recv(SIZE, ENCODING))
     print(f"[Client] receive response {commonValues}")
-
-    # u = user index
-    u = json.loads(s.recv(SIZE, ENCODING))
-    print(f"[Client] receive user index {u}")
 
     s.close()
 
@@ -64,9 +60,17 @@ def shareKeys():
     # t = threshold, u = user index
     # request = [[u, v1, euv], [u, v2, euv], ...]
     request = []
-    response = []
+    # response = []
+    response = {}
 
-    euv_list, bu = sa.generateSharesOfMask(commonValues["t"], u, my_keys["s_sk"], my_keys["c_sk"], others_keys)
+    for i, user_dic in others_keys.items():
+        if my_keys["c_pk"] == user_dic["c_pk"] and my_keys["s_pk"] == user_dic["s_pk"]:
+            u = user_dic["index"]  # u = user index
+            break
+        else:
+            continue
+
+    euv_list, bu = sa.generateSharesOfMask(commonValues["t"], u, my_keys["s_sk"], my_keys["c_sk"], others_keys, commonValues["R"])
     request = euv_list
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -83,7 +87,11 @@ def shareKeys():
     print(f"[Client] receive response {response}")
 
     # store euv from server to client in dic
-    for v, euv in enumerate(response):  # example response = ["e01", "e11"]
+    """for v, euv in enumerate(response):  # example response = ["e01", "e11"]
+        others_euv[v] = euv
+    """
+    # if response format {v: euv}. example = {0: "e00", 1: "e10"}
+    for v, euv in response.items():
         others_euv[v] = euv
 
     s.close()
@@ -97,9 +105,8 @@ def MaskedInputCollection():
     for i, user_dic in others_keys.items():
         v = i
         s_pk_dic[i] = user_dic.get("s_pk")
-    yu = sa.generateMaskedInput(u, bu, xu, my_keys["s_sk"], euv_list, s_pk_dic)
-
-    request = {u: yu}  # example: {0: y0}
+    yu = sa.generateMaskedInput(u, bu, xu, my_keys["s_sk"], euv_list, s_pk_dic, commonValues["R"])
+    request = {u: yu}  # request example: {0: y0}
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(HOST, PORT)
@@ -109,14 +116,41 @@ def MaskedInputCollection():
     s.sendall(bytes(json.dumps(request), ENCODING))
     print(f"[Client] send request {request}")
 
-    # receive yu_list from server
+    # receive sending_yu_list from server
     response = json.loads(s.recv(SIZE, ENCODING))
     print(f"[Client] receive response {response}")
-    others_yu = response
+
+    # U3 = survived users in round2(MaskedInputCollection) = users_last used in round4(unmasking)
+    U3 = response
 
     s.close()
 
 
 def Unmasking():
-    #temp
-    return 0
+    request = {}
+    s_sk_shares_dic = {}
+    bu_shares_dic = {}
+    temp_request = (s_sk_shares_dic, bu_shares_dic)
+
+    c_pk_dic = {}
+    for i, user_dic in others_keys.items():
+        v = i
+        c_pk_dic[i] = user_dic.get("c_pk")
+
+    # U2 = survived users in round1(shareKeys) = users_previous
+    U2 = list(others_euv.keys())
+
+    # requests example: {u: [{0: s02_sk, 1: s03_sk, ...}, {1: b01, 4: b04, ...}]}
+    temp_request = sa.unmasking(u, my_keys["c_sk"], euv_list, c_pk_dic, U2, U3)
+    request = {u: list(temp_request)}
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(HOST, PORT)
+    print("connected!")
+
+    # send u and dropped users' s_sk, survived users' bu in json format
+    s.sendall(bytes(json.dumps(request), ENCODING))
+    print(f"[Client] send request {request}")
+
+    s.close()
+

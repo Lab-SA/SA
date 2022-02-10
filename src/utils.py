74739abd3274997952370656b04554eeb0810a60
mimwin <kms105903@naver.com>
2022-02-10 22:06:36 +0900

    feat: make function about average_weight
    utils.py : seperate functions about average_weight
    options.py : set parser based on testcode environment

--- src/utils.py
+++ src/utils.py
@@ -49,13 +49,32 @@ def average_weights(w):
     """
     Returns the average of the weights.
     """
+    w_avg = copy.deepcopy(w)
+    for key in w_avg.keys():
+        w_avg[key] = torch.div(w_avg[key], len(w))
+    return w_avg
+
+def sum_weights(w):
+    """
+    Returns the sum of the weights.
+    """
+    w_sum = copy.deepcopy(w[0])
+    for key in w_sum.keys():
+        for i in range(1, len(w)):
+            w_sum[key] += w[i][key]
+    return w_sum
+
+
+def average_weights_origin(w):
+    """
+    Returns the average of the weights.
+    """
     w_avg = copy.deepcopy(w[0])
     for key in w_avg.keys():
         for i in range(1, len(w)):
             w_avg[key] += w[i][key]
         w_avg[key] = torch.div(w_avg[key], len(w))
     return w_avg
-
 
 def exp_details(args):
     print('\nExperimental details:')

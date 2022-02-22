aaeb4b4928bbc326d8090ea3af54398e4ad8be29
Sub1nJ0 <77657793+Sub1nJ0@users.noreply.github.com>
2022-02-22 22:40:10 +0900

    Turbo protocol : change reconstruct
    reconstruct 보간다항식 g_i 수정완료

--- Turbo.py
+++ Turbo.py
@@ -17,19 +17,22 @@ def grouping(users, n):
 
 
 # compute tildeX = x + u_i + r_ij
-def computeMaskedModel(x, u_i, r_ij_dic):
+def computeMaskedModel(x, u_i, next_users, q):
     # x = local model
     # u_i 1~R random mask vector from server
     # tildeX_dic = [dic] masked model(=tildeX) between user i(this) and users in l-1
     tildeX_dic = {}
+    r_ij_dic = additiveMasking(next_users, q)
+    print(f"r_ij_dic: {r_ij_dic}")
 
-    for j in range(len(r_ij_dic)):
-        tildeX_dic[j] = x + u_i + r_ij_dic[j]
+    for j in r_ij_dic.keys():
+        temp = x + u_i + r_ij_dic[j]
+        tildeX_dic[j] = temp
 
     return tildeX_dic
 
 
-def additiveMasking(i, next_users, q):
+def additiveMasking(next_users, q):
     # next_users = users' index in other group (= j)
     # r_ij_dic = additive random vector dictionary by user i
     n = len(next_users)
@@ -41,45 +44,51 @@ def additiveMasking(i, next_users, q):
         r_ij_dic[next_users[j]] = r_ij
 
     temp_sum = sum(r_ij_dic.values())
-    r_ij_dic[next_users[n-1]] = 0 - temp_sum
+    temp_r = 0 - temp_sum
+    r_ij_dic[next_users[n - 1]] = temp_r
 
     return r_ij_dic
 
 
 # update aggregate value tildeS
-def updateSumofMaskedModel(l, n, pre_tildeS_dic, tildeX_dic, p_sum_dic):
+def updateSumofMaskedModel(l, n, pre_tildeX_dic, pre_tildeS_dic):
     # l = group index of this user.  ex) 0,1,2,...,L
-    # tildeS = a variable that each user holds corresponding to the aggregated masked models from the previous group
     # n = the number of users per one group
     # pre_tildeS_dic = [dic] tildeS of users in l-1 group
     # tildeX_dic = [dic] masked model(=tildeX) between user i(this) and users in l-1
+    # tildeS = a variable that each user holds corresponding to the aggregated masked models from the previous group
+    tildeS = 0
 
+    """
+        initialize tildeS(0) = 0
+        매개변수 때문에, group index 가 0일 때(즉 첫 번째 group 일 경우)는 사용자 측에서
+        매개변수로 p_sum = 0, pre_tildeS = 0을 넣어 주어야 할 듯.
+    """
     if l == 0:
-        p_sum_dic[l] = 0
-        p_sum_dic[l+1] = computePartialSum(l, n, pre_tildeS_dic)
-        tildeS = 0
+        print("Initialize tildeS(0) = 0")
     elif l > 0:
-        tildeS = sum(pre_tildeS_dic.values()) / n + sum(tildeX_dic.values())
-        p_sum_dic[l+1] = computePartialSum(l, n, pre_tildeS_dic)
+        tildeS = sum(pre_tildeS_dic.values()) / n + sum(pre_tildeX_dic.values())
     else:
         print("wrong group index")
 
-    return tildeS, p_sum_dic
+    return tildeS
 
 
-# compute partial summation = p_sum (= s(l+1))
+# compute partial summation = p_sum (= s(l))
 def computePartialSum(l, pre_tildeS_dic):
-    # initial partial sum s(1), s(2) = 0
-
-    p_sum = sum(pre_tildeS_dic.values()) / len(pre_tildeS_dic)
+    # initial partial sum s(0), s(1) = 0
+    if l == 0:
+        print("Initialize p_sum(0) = 0")
+    elif l > 0:
+        p_sum = sum(pre_tildeS_dic.values()) / len(pre_tildeS_dic)
+    else:
+        print("wrong group index")
 
     return p_sum
 
-
 # generate the encoded model barX
-def generateEncodedModel(next_users, alpha_list, beta_list, tildeX_dic):
+def generateEncodedModel(alpha_list, beta_list, tildeX_dic):
     """
-        next_users = users' index of l+1 group
         x = 다음 그룹의 유저들 각각에 랜덤 할당된 수 (= alpha)
         y = tildeX_dic.values()
         이 (x,y)로 만들어진 보간다항식 = f_i
@@ -98,15 +107,16 @@ def generateEncodedModel(next_users, alpha_list, beta_list, tildeX_dic):
 
     return barX_dic
 
-# generate random vector alpha_list, beta_list
+
 def generateRandomVectorSet(next_users, q):
+    # next_users = users' index of l+1 group
     n = len(next_users)
     alpha_list = random.sample(range(1, q), n)
     beta_list = []
 
     for j in next_users:
-        beta = random.randrange(1, q)
         while True:
+            beta = random.randrange(1, q)
             if beta not in alpha_list and beta not in beta_list:
                 beta_list.append(beta)
                 break
@@ -120,8 +130,8 @@ def generateLagrangePolynomial(x_list, y_list):
     if 1. generate f_i of user i in group l (this client),
         x_list = alpha_list, y_list = list(tildeX_dic.values())
 
-    if 2. generate f_k of user k in group l-1 (reconstruct)
-        x_list = alphat_list + beta_list, y_list = list(tildeX_dic.values()) + list(barX_dic.values())
+    if 2. generate g_i of user k in group l-1 (reconstruct)
+        x_list = alphat_list + beta_list, y_list = list(tildeS_dic.values()) + list(barS_dic.values())
 
     return Lagrange Polynomial f_i
     """
@@ -142,31 +152,32 @@ def updateSumofEncodedModel(l, pre_barX_dic, p_sum_dic):
     barS = p_sum_dic[l] + sum(pre_barX_dic.values())
     return barS
 
+
 # reconstruct missing values of dropped users.
 # and then update pre_tildeX
-def reconstruct(alpha_list, beta_list, pre_tildeX, pre_barX):
-    # pre_tildeX = [dic] { surviving users'(group l-1) index: surviving users' tildeX }
-    # pre_barX = [dic] { surviving users'(group l-1) index: surviving users' barX }
-    # f_k = lagrange polynomial of group l-1 for reconstruct
+def reconstruct(alpha_list, beta_list, pre_tildeS_dic, pre_barS_dic):
+    # pre_tildeS_dic = [dic] { surviving users'(group l-1) index: surviving users' tildeS }
+    # pre_barS_dic = [dic] { surviving users'(group l-1) index: surviving users' barS }
+    # g_i = lagrange polynomial of group l-1 for reconstruct
+    x_list = []
 
-    x_list = list(pre_tildeX.keys()) + list(pre_barX.keys())
-    for i in pre_tildeX.keys():
+    for i in pre_tildeS_dic.keys():
         x_list.append(alpha_list[i])
-    for i in pre_barX.keys():
+    for i in pre_barS_dic.keys():
         x_list.append(beta_list[i])
-    y_list = list(pre_tildeX.values()) + list(pre_barX.values())
+    y_list = list(pre_tildeS_dic.values()) + list(pre_barS_dic.values())
 
-    f_k = generateLagrangePolynomial(x_list, y_list)
+    g_i = generateLagrangePolynomial(x_list, y_list)
 
     for index in range(len(alpha_list)):
         alpha = alpha_list[index]
         if alpha not in x_list:
-            recon_tildeX = np.polyval(f_k, alpha)
-            pre_tildeX[index] = recon_tildeX
+            recon_tildeS = np.polyval(g_i, alpha)
+            pre_tildeS_dic[index] = recon_tildeS
         else:
             continue
 
-    return pre_tildeX
+    return pre_tildeS_dic
 
 
 def computeFinalOutput(x, final_tildeS, u_i_dic):
@@ -175,3 +186,4 @@ def computeFinalOutput(x, final_tildeS, u_i_dic):
 
     sum_x = sum(final_tildeS.values()) / len(final_tildeS) - sum(u_i_dic)
     return sum_x
+

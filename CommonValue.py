f14a9835b94261da13a5a3c638bde22524653c65
eigenkyeong <kokoyou163@gmail.com>
2022-03-29 18:47:16 +0900

    Add BreaRound

--- CommonValue.py
+++ CommonValue.py
@@ -6,3 +6,16 @@ class BasicSARound(Enum):
     ShareKeys = 7020
     MaskedInputCollection = 7030
     Unmasking = 7040
+
+class TurboRound(Enum):
+    SetUp = 7000
+    Turbo = 7010
+    TurboValue = 7011
+    TurboFinal = 7012
+    Final = 7020
+
+class BreaRound(Enum):
+    SetUp = 7000
+    SecretSharing = 7010
+    SelectUsers = 7020
+    Unmasking = 7030

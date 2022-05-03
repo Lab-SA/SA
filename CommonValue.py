6475ade49cd4f41142094bd8ea8545aca65333d4
eigenkyeong <kokoyou163@gmail.com>
2022-05-04 04:34:33 +0900

    Brea: delete BreaRound

--- CommonValue.py
+++ CommonValue.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class BasicSARound(Enum):
-    SetUp = 7000
-    AdvertiseKeys = 7010
-    ShareKeys = 7020
-    MaskedInputCollection = 7030
-    Unmasking = 7040
+    SetUp = 1
+    AdvertiseKeys = 2
+    ShareKeys = 3
+    MaskedInputCollection = 4
+    Unmasking = 5
 
 class TurboRound(Enum):
     SetUp = 7000
@@ -14,8 +14,3 @@ class TurboRound(Enum):
     TurboFinal = 7012
     Final = 7020
 
-class BreaRound(Enum):
-    SetUp = 7000
-    SecretSharing = 7010
-    SelectUsers = 7020
-    Unmasking = 7030

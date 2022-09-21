b12af5747b8eef4f0f8a28cbaaf778445d78c970
0JIEUN0 <je300512@gmail.com>
2022-09-21 12:43:55 +0900

    feat: change learning rate and data size per user

--- learning/sampling.py
+++ learning/sampling.py
@@ -14,12 +14,12 @@ def mnist_iid(dataset, num_users):
     :param num_users:
     :return: dict of image index
     """
-    num_items = int(len(dataset)/num_users)
+    num_items = 5000 #int(len(dataset)/num_users)
     dict_users, all_idxs = {}, [i for i in range(len(dataset))]
     for i in range(num_users):
         dict_users[i] = set(np.random.choice(all_idxs, num_items,
                                              replace=False))
-        all_idxs = list(set(all_idxs) - dict_users[i])
+        #all_idxs = list(set(all_idxs) - dict_users[i])
     return dict_users
 
 

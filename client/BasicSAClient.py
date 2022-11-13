11190b98d14857feb8f4bd94666fa4edee00ddfd
GIRL-0 <63105233+GIRL-0@users.noreply.github.com>
2022-11-14 00:01:38 +0900

    fix(basicSA): fix client key and others_euv

--- client/BasicSAClient.py
+++ client/BasicSAClient.py
@@ -1,4 +1,5 @@
 import json, socket, os, sys
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import BasicSA as sa
 from CommonValue import BasicSARound
@@ -7,6 +8,7 @@ import learning.models_helper as mhelper
 
 SIZE = 2048
 ENCODING = 'utf-8'
+
 
 def sendRequest(host, port, tag, request):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,7 +29,7 @@ def sendRequestAndReceive(host, port, tag, request):
 
     # add tag to request
     request['request'] = tag
-    
+
     # send request
     s.sendall(bytes(json.dumps(request) + "\r\n", ENCODING))
     # print(f"[{tag}] Send request")
@@ -63,10 +65,10 @@ class BasicSAClient:
     # xu = 0  # temp. local model of this client
 
     commonValues = {}  # {"n": n, "t": t, "g": g, "p": p, "R": R} from server in setup stage
-    u = 0  # u = user index
-    my_keys = {}  # c_pk, c_sk, s_pk, s_sk of this client
-    s_pk_dic = {}       # other users' s_pk(public key) dic
-    c_pk_dic = {}       # other users' c_pk(public key) dic
+    u = -1  # u = user index
+    my_c_pk = my_c_sk = my_s_pk = my_s_sk = 0  # c_pk, c_sk, s_pk, s_sk of this client
+    s_pk_dic = {}  # other users' s_pk(public key) dic
+    c_pk_dic = {}  # other users' c_pk(public key) dic
     euv_list = []  # euv of this client
     others_euv = {}
     bu = 0  # random element to be used as a seed for PRG
@@ -80,30 +82,30 @@ class BasicSAClient:
         response = sendRequestAndReceive(self.HOST, self.PORT, tag, {})
         self.commonValues = response
         self.u = response["index"]
-        self.data = response["data"] # user_groups[idx]
+        self.data = response["data"]  # user_groups[idx]
         global_weights = mhelper.dic_of_list_to_weights(response["weights"])
 
         if self.model == {}:
             self.model = fl.setup()
         fl.update_model(self.model, global_weights)
-        local_model, local_weight, local_loss = fl.local_update(self.model, self.data, 0) # epoch 0 (temp)
+        local_model, local_weight, local_loss = fl.local_update(self.model, self.data, 0)  # epoch 0 (temp)
         self.weight = local_weight
 
     def advertiseKeys(self):
         tag = BasicSARound.AdvertiseKeys.name
 
         (c_pk, c_sk), (s_pk, s_sk) = sa.generateKeyPairs()
-        self.my_keys["c_pk"] = c_pk
-        self.my_keys["c_sk"] = c_sk 
-        self.my_keys["s_pk"] = s_pk
-        self.my_keys["s_sk"] = s_sk
+        self.my_c_pk = c_pk
+        self.my_c_sk = c_sk
+        self.my_s_pk = s_pk
+        self.my_s_sk = s_sk
         request = {"index": self.u, "c_pk": c_pk, "s_pk": s_pk}
 
         # send {"c_pk": c_pk, "s_pk": s_pk} to server in json format
         # receive other users' public keys from server in json format
         response = sendRequestAndReceive(self.HOST, self.PORT, tag, request)
 
-        # store response on client
+        # store response on clients
         # example: {"0": {"c_pk": "2123", "s_pk": "3333", "index": 0}, "1": {"c_pk": "1111", "s_pk": "2222", "index": 1}}
         self.s_pk_dic = {}
         self.c_pk_dic = {}
@@ -112,17 +114,18 @@ class BasicSAClient:
             self.s_pk_dic[v] = user_dic["s_pk"]
             self.c_pk_dic[v] = user_dic["c_pk"]
 
-
     def shareKeys(self):
         tag = BasicSARound.ShareKeys.name
 
         # t = threshold, u = user index
         # request = [[u, v1, euv], [u, v2, euv], ...]
         self.euv_list, self.bu = sa.generateSharesOfMask(
-            self.commonValues["t"], 
+            self.commonValues["t"],
             self.u,
-            self.my_keys["s_sk"], 
-            self.my_keys["c_sk"], 
+            # self.my_keys["s_sk"],
+            # self.my_keys["c_sk"],
+            self.my_s_sk,
+            self.my_c_sk,
             self.c_pk_dic,
             self.commonValues["R"]
         )
@@ -136,23 +139,24 @@ class BasicSAClient:
             others_euv[v] = euv
         """
         # if response format {v: euv}. example = {0: "e00", 1: "e10"}
+        self.others_euv = {}
         for v, euv in response.items():
             self.others_euv[int(v)] = euv
-
 
     def maskedInputCollection(self):
         tag = BasicSARound.MaskedInputCollection.name
 
         yu = sa.generateMaskedInput(
-            self.u, 
-            self.bu, 
+            self.u,
+            self.bu,
             self.weight,
-            self.my_keys["s_sk"], 
-            self.euv_list, 
+            # self.my_keys["s_sk"],
+            self.my_s_sk,
+            self.euv_list,
             self.s_pk_dic,
             self.commonValues["R"]
         )
-        
+
         request = {"index": self.u, "yu": mhelper.weights_to_dic_of_list(yu)}  # request example: {"idx":0, "yu":y0}
 
         # receive sending_yu_list from server
@@ -161,7 +165,6 @@ class BasicSAClient:
         # U3 = survived users in round2(MaskedInputCollection) = users_last used in round4(unmasking)
         self.U3 = response['users']
 
-
     def unmasking(self):
         tag = BasicSARound.Unmasking.name
 
@@ -169,11 +172,12 @@ class BasicSAClient:
         U2 = list(self.others_euv.keys())
 
         s_sk_shares_dic, bu_shares_dic = sa.unmasking(
-            self.u, 
-            self.my_keys["c_sk"], 
-            self.others_euv, 
+            self.u,
+            # self.my_keys["c_sk"],
+            self.my_c_sk,
+            self.others_euv,
             self.c_pk_dic,
-            U2, 
+            U2,
             self.U3
         )
         # requests example: {"idx": 0, "ssk_shares": {2: s20_sk, 3: s30_sk, ...}, "bu_shares": {1: b10, 4: b40, ...}]}
@@ -182,12 +186,13 @@ class BasicSAClient:
         # send u and dropped users' s_sk, survived users' bu in json format
         sendRequestAndReceive(self.HOST, self.PORT, tag, request)
 
-if __name__ == "__main__":
-    client = BasicSAClient() # test
 
-    for i in range(5): # 5 rounds
+if __name__ == "__main__":
+    client = BasicSAClient()  # test
+
+    for i in range(5):  # 5 rounds
         client.setUp()
         client.advertiseKeys()
-        client.shareKeys()    
+        client.shareKeys()
         client.maskedInputCollection()
         client.unmasking()
